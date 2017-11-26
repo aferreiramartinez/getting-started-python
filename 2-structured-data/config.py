4541cacb9a6cf530efc89a959dc0a0d6244c023f
Tony <ant1bball@gmail.com>
2017-11-26 14:30:39 +0100

    add credentials and mongoDb config

--- 2-structured-data/config.py
+++ 2-structured-data/config.py
@@ -28,11 +28,11 @@ SECRET_KEY = 'secret'
 # configure the respective settings for the one you choose below.
 # You do not have to configure the other data backends. If unsure, choose
 # 'datastore' as it does not require any additional configuration.
-DATA_BACKEND = 'datastore'
+DATA_BACKEND = 'mongodb'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'your-project-id'
+PROJECT_ID = 'pythonfundamentals'
 
 # CloudSQL & SQLAlchemy configuration
 # Replace the following values the respective values of your Cloud SQL
@@ -76,4 +76,4 @@ else:
 # If using mongolab, the connection URI is available from the mongolab control
 # panel. If self-hosting on compute engine, replace the values below.
 MONGO_URI = \
-    'mongodb://user:password@host:27017/database'
+    'mongodb://aferreiramartinez:Waxqptp55@ds119436.mlab.com:19436/convector-labs-db'
