e618a8901c7776491e7512c7263cb0bde77d9f15
Tony <ant1bball@gmail.com>
2018-05-30 07:55:40 +0200

    new data for estimates and reports, add currency, add timestamp, FQ end date

--- 2-structured-data/dailyUpdate.ps1
+++ 2-structured-data/dailyUpdate.ps1
@@ -1,1 +1,7 @@
-﻿python test.py
+﻿virtualenv --python "C:\Users\ant1_\AppData\Local\Programs\Python\Python36-32\python.exe" env
+env\scripts\activate
+pip install lxml
+pip install -r requirements.txt
+python test.py
+
+python test.py
