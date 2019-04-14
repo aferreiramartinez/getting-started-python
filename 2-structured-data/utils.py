798fbfe9d799c2fada656a668790dc3e64549321
aferreiramartinez <ant1bball@gmail.com>
2019-04-14 23:50:57 +0200

    add file with full company names

--- 2-structured-data/utils.py
+++ 2-structured-data/utils.py
@@ -1,16 +1,20 @@
 from database import model_mongodb
 
 if __name__ == '__main__':
+    #REVERSE OBSERVATIONS
+    #cursor = model_mongodb.read_all()
+    #counter=1
+    #for document in cursor:
+        #docId = document['_id']
+        #myList=document["observations"]
+        #document["observations"] = list(reversed(myList))
+        #model_mongodb.update(document, str(docId))
 
     cursor = model_mongodb.read_all()
-    counter=1
-    for document in cursor:
-        print(counter)
-        counter+=1
-        docId = document['_id']
-        myList=document["observations"]
-        document["observations"] = list(reversed(myList))
-        model_mongodb.update(document, str(docId))
-
-
+    with open('SP500-ticker-and-names.txt', 'a') as the_file:
+        for document in cursor:
+            ticker=document["Ticker"]
+            companyName=document["CompanyName"]
+            the_file.write(ticker+' - '+companyName+'\n')
+        the_file.close()
 
