c129e2360853dd081a5538daee6f2c96dd5bd3d9
aferreiramartinez <ant1bball@gmail.com>
2020-04-03 21:07:14 +0200

    conditional update visible alpha

--- 2-structured-data/MongoDbFunctions/SyncVAandEikonConditional.js
+++ 2-structured-data/MongoDbFunctions/SyncVAandEikonConditional.js
@@ -2,7 +2,7 @@ db.visibleAlphaLatest.find().forEach(function(doc){
     var ticker = doc.EikonTicker
     print(ticker)
     db.eikonTwo.update(
-	    { "EikonTicker": ticker},
+	    { "EikonTicker": ticker, "VisibleAlpha" : {$exists : false}},
 	    { $set: { "VisibleAlpha.DataByFiscalQuarter": doc.DataByFiscalQuarter,  "VisibleAlpha.DataByFiscalYear": doc.DataByFiscalYear } }
 	)
 });
